d85ef9410e8d5259c942707f5627201bb5f28d61
dhanaseelan77 <panneer3998@gmail.com>
2026-06-27 15:53:37 +0530

    Add login form submission handling

--- js/login.js
+++ js/login.js
@@ -1,1 +1,19 @@
 
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+
+    e.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "student" && password === "12345") {
+
+        localStorage.setItem("login", "true");
+        window.location.href = "home.html";
+
+    } else {
+
+        document.getElementById("error").innerHTML = "Invalid Username or Password";
+    }
+
+});
