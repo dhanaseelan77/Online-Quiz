579ec3d0c2081b3c3bec7228d0b5f987326cded3
dhanaseelan77 <panneer3998@gmail.com>
2026-06-27 15:55:36 +0530

    Display quiz results and user answers

--- js/result.js
+++ js/result.js
@@ -1,1 +1,36 @@
 
+let category = localStorage.getItem("category");
+let quizQuestions = questions[category];
+
+let answers = JSON.parse(localStorage.getItem("answers"));
+let score = localStorage.getItem("score");
+
+document.getElementById("score").innerHTML =
+"Your Score: " + score + " / " + quizQuestions.length;
+
+let output = "";
+
+quizQuestions.forEach((q, index) => {
+
+    output += "<div class='question'>";
+    output += "<h3>" + (index + 1) + ". " + q.question + "</h3>";
+
+    if (answers[index] === q.answer) {
+
+        output += "<p class='correct'>✔ Your Answer: " + answers[index] + "</p>";
+
+    } else {
+
+        output += "<p class='wrong'>✘ Your Answer: " +
+        (answers[index] || "Not Answered") + "</p>";
+
+        output += "<p class='correct'>✔ Correct Answer: " +
+        q.answer + "</p>";
+
+    }
+
+    output += "</div>";
+
+});
+
+document.getElementById("result").innerHTML = output;
